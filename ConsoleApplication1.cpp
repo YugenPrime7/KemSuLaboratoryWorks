9d434d25150dbe37a5a6819e8be410f0265814fb
YugenPrime7 <firoxary7@gmail.com>
2026-09-22 14:56:27 +0700

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,14 +1,13 @@
-﻿/*************************
-* Автор: Горбунов Р.А. *
-* Вариант: 4 *
+﻿/*******************************
+* Автор: Горбунов Р.А.         *
+* Вариант: 4                   *
 * Название: Линейные алгоритмы *
-*************************/
+********************************/
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 	double y, a, b, T1, T2, k, n1, n2, n3;
 
 	cout << "Degree of volume reduction: ";
@@ -26,14 +25,14 @@ int main()
 	cout << "Process temperature(2): ";
 	cin >> T2;
 
-	k = (y - 1) / y;
+	k = (y - 1.0) / y;
 	n1 = 1 - (pow((1 / b), k));
-	n2 = (T2 - T1) / (T2 + ((T2 - T1) / ((y - 1) * (std::log(a)))));
-	n3 = 1 - k * ((std::log(b)) / (pow(b, k) - 1));
+	n2 = (T2 - T1) / (T2 + ((T2 - T1) / ((y - 1) * (log(a)))));
+	n3 = 1 - k * ((log(b)) / (pow(b, k) - 1));
 
 	cout << n1 << endl
-		<< n2 << endl
-		<< n3 << endl;
+         << n2 << endl
+         << n3 << endl;
 
 	return 0;
 }
